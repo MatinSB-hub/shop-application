2bb604008ebb26a01a5d67f30f78674458021d0e
MatinSB-hub <matin.sa200@gmail.com>
2026-08-13 21:57:52 +0430

    feat: add create product modal

--- src/Pages/DashBoard/templates/moderator/Products/ModeratorProductsTable.jsx
+++ src/Pages/DashBoard/templates/moderator/Products/ModeratorProductsTable.jsx
@@ -1,31 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Table from "../../../../../Components/Templates/Dashboard/common/Table";
+import TableToolbar from "../../../../../Components/Templates/Dashboard/common/Table/TableToolbar";
+import TableRow from "../../../../../Components/Templates/Dashboard/common/Table/TableRow";
+import TableBody from "../../../../../Components/Templates/Dashboard/common/Table/TableBody";
+import TableHead from "../../../../../Components/Templates/Dashboard/common/Table/TableHead";
+import TableCell from "../../../../../Components/Templates/Dashboard/common/Table/TableCell";
+import { BiPlus } from "react-icons/bi";
+import ProductDrawer from "../../../../../Components/Templates/Dashboard/common/ProductDrawer";
 
 function ModeratorProductsTable() {
+  const [isDrawerShow, setIsDrawerShow] = useState(false);
+  const toggleDrawer = () => setIsDrawerShow((prev) => !prev);
   return (
-    <Table>
-      <TableToolbar>
-        <div>
-          <h2>تمامی محصولات</h2>
-        </div>
-      </TableToolbar>
-      <TableHead>
-        <TableRow>
-          <TableCell>شناسه</TableCell>
-          <TableCell>عنوان</TableCell>
-          <TableCell>مبلغ</TableCell>
-          <TableCell>وضعیت</TableCell>
-        </TableRow>
-      </TableHead>
+    <>
+      <Table>
+        <TableToolbar useFlexBetween>
+          <div>
+            <h2>تمامی محصولات</h2>
+          </div>
 
-      <TableBody>
-        <TableRow>
-          <TableCell></TableCell>
-          <TableCell></TableCell>
-          <TableCell></TableCell>
-          <TableCell></TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+          <div className="flex items-center gap-3">
+            <button
+              onClick={toggleDrawer}
+              className="px-3 hover:opacity-90 flex items-center h-10 rounded-md bg-blue-500 text-white"
+            >
+              <BiPlus />
+              <span>ایجاد محصول</span>
+            </button>
+          </div>
+        </TableToolbar>
+        <TableHead>
+          <TableRow>
+            <TableCell>شناسه</TableCell>
+            <TableCell>عنوان</TableCell>
+            <TableCell>مبلغ</TableCell>
+            <TableCell>وضعیت</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          <TableRow>
+            <TableCell>cd53se</TableCell>
+            <TableCell>laptop asus</TableCell>
+            <TableCell>{150_000_00}</TableCell>
+            <TableCell>public</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <ProductDrawer isOpen={isDrawerShow} onToggle={toggleDrawer}/>
+    </>
   );
 }
 
--- src/Pages/DashBoard/ui/PageLable.jsx
+++ src/Pages/DashBoard/ui/PageLable.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-function PageLable() {
+function PageLable({lable}) {
   return (
-    <div>pageLable</div>
+    <div className="w-full h-0.5 bg-zinc-300 flex justify-center items-center rounded-2xl"s>
+      <span className="px-10 py-3 bg-zinc-50 text-sm">{lable}</span>
+    </div>
   )
 }
 
