8751d5d3e2b1d2f4c1dc0ef861a08118bfd3d8f3
MatinSB-hub <matin.sa200@gmail.com>
2026-08-25 15:21:15 +0430

    feat: add Confirm component for product deletion

--- src/Pages/DashBoard/templates/moderator/Products/ModeratorProductsTable.jsx
+++ src/Pages/DashBoard/templates/moderator/Products/ModeratorProductsTable.jsx
@@ -6,16 +6,25 @@ import TableBody from "../../../../../Components/Templates/Dashboard/common/Tabl
 import TableHead from "../../../../../Components/Templates/Dashboard/common/Table/TableHead";
 import TableCell from "../../../../../Components/Templates/Dashboard/common/Table/TableCell";
 import { BiPlus } from "react-icons/bi";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { MdOutlineModeEdit } from "react-icons/md";
 import ProductDrawer from "../../../../../Components/Templates/Dashboard/common/ProductDrawer";
 import useProducts from "../../../../../hooks/useProducts";
 import { formatPrice, getDisplayPrice } from "../../../../../lib/helpers/price";
+import Confirm from "../../../../../Components/Common/Confirm";
 
 function ModeratorProductsTable() {
+  const [DeletingProduct, setDeletingProduct] = useState();
+  const [isDeleting, setIsDeleting] = useState(null);
   const [isDrawerShow, setIsDrawerShow] = useState(false);
   const toggleDrawer = () => setIsDrawerShow((prev) => !prev);
 
   const { products, pagination, page, setPage, isLoading, error } =
     useProducts();
+
+  const handleRemove = () => {
+    setIsDeleting(true)
+  };
 
   return (
     <>
@@ -40,6 +49,7 @@ function ModeratorProductsTable() {
             <TableCell>شناسه</TableCell>
             <TableCell>عنوان</TableCell>
             <TableCell>مبلغ</TableCell>
+            <TableCell>وضعیت</TableCell>
           </TableRow>
         </TableHead>
         {isLoading && (
@@ -83,31 +93,50 @@ function ModeratorProductsTable() {
                     {formatPrice(price)} تومان
                     {hasMultipleSellers && " (چند فروشنده دارد)"}
                   </TableCell>
+                  <TableCell>
+                    <button className="text-blue-400 hover:bg-blue-100 p-2 rounded-md">
+                      <MdOutlineModeEdit />
+                    </button>
+                    <button
+                      className="text-red-400 hover:bg-red-100 p-2 rounded-md"
+                      title="حذف"
+                      onClick={() => setDeletingProduct(product)}
+                    >
+                      <FaRegTrashCan />
+                    </button>
+                  </TableCell>
                 </TableRow>
               );
             })}
         </TableBody>
 
-        <div className="flex items-center justify-center gap-2 mt-4">
-          <button
-            className="px-3 py-1 rounded-md primary-border text-sm disabled:opacity-40"
-            disabled={page <= 1}
-            onClick={() => setPage((prev) => prev - 1)}
-          >
-            قبلی
-          </button>
-            {console.log("pagination:",pagination)}
-          <span>
-            صفحه {pagination.page} از {pagination.tottalPages}
-          </span>
-          <button
-            className="px-3 py-1 rounded-md primary-border text-sm disabled:opacity-40"
-            disabled={page > pagination.totalPages}
-            onClick={() => setPage((prev) => prev + 1)}
-          >
-            بعدی
-          </button>
-        </div>
+        {pagination && pagination.totalPrice > 1 && (
+          <div className="flex items-center justify-center gap-2 mt-4">
+            <button
+              className="px-3 py-1 rounded-md primary-border text-sm disabled:opacity-40"
+              disabled={page <= 1}
+              onClick={() => setPage((prev) => prev - 1)}
+            >
+              قبلی
+            </button>
+            {console.log("pagination:", pagination)}
+            <span>
+              صفحه {pagination.page} از {pagination.tottalPages}
+            </span>
+            <button
+              className="px-3 py-1 rounded-md primary-border text-sm disabled:opacity-40"
+              disabled={page > pagination.totalPages}
+              onClick={() => setPage((prev) => prev + 1)}
+            >
+              بعدی
+            </button>
+          </div>
+        )}
+
+        <Confirm
+          isOpen={!!DeletingProduct}
+          title="حذف محصول"
+        />
       </Table>
       <ProductDrawer isOpen={isDrawerShow} onToggle={toggleDrawer} />
     </>
