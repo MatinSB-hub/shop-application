197b3a0dfda22e258432637aa6423abec3494785
MatinSB-hub <matin.sa200@gmail.com>
2026-08-27 15:13:06 +0430

    feat(category.services): complete category and subCategory API methods

--- src/services/category.sevices.js
+++ src/services/category.sevices.js
@@ -1,6 +1,44 @@
-import api from "./api"
+import api from "./api";
 
+//Category
 export const getAllCategories = async () => {
-    const {data} = await api.get("/category")
-    return data
-}
+  const { data } = await api.get("/category");
+  return data;
+};
+
+export const createCategory = async (formData) => {
+  const { data } = await api.post("/category", { formData });
+  return data;
+};
+
+export const updateCategory = async (categoryId, formData) => {
+  const { data } = await api.put(`/category/${categoryId}`, { formData });
+  return data;
+};
+
+export const deleteCategory = async (categoryId) => {
+  const { data } = await api.delete(`/category/${categoryId}`);
+  return data;
+};
+
+
+//subCategory
+export const getAllSubCategories = async () => {
+  const { data } = await api.get("/category/sub");
+  return data;
+};
+
+export const createSubCategory = async (formData) => {
+  const { data } = await api.post("/category/sub", { formData });
+  return data;
+};
+
+export const updateSubCategory = async (categoryId, formData) => {
+  const { data } = await api.put(`/category/sub/${categoryId}`, { formData });
+  return data;
+};
+
+export const removeSubCategory = async (categoryId) => {
+  const { data } = await api.delete(`/category/sub/${categoryId}`);
+  return data;
+};
