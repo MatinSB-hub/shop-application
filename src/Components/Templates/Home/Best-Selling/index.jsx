e45e1539c56da19821cc4944414e838de69368f0
MatinSB-hub <matin.sa200@gmail.com>
2026-09-07 12:58:01 +0430

    feat(homepage): add swiper silde for best selling

--- src/Components/Templates/Home/Best-Selling/index.jsx
+++ src/Components/Templates/Home/Best-Selling/index.jsx
@@ -5,11 +5,14 @@ import useProducts from "../../../../hooks/useProducts";
 import { Link } from "react-router";
 import { array } from "zod";
 import ProductCardSkeleton from "./components/ProductCardSkeleton";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Slider from "../../Product/Content/RelatedProducts/Slider";
+import { Autoplay } from "swiper/modules";
 
 const BestSelling = () => {
   const { products, isLoading, error } = useProducts();
   return (
-    <section className="container my-[50px]">
+    <section className="container my-12.5">
       <SectionTitle
         text="پرفروش‌ترین ها"
         action={
@@ -21,21 +24,46 @@ const BestSelling = () => {
           </Link>
         }
       />
-      <div className="mt-10 w-full border rounded-2xl grid grid-cols-5 p-4 border-neutral-300 divide-x divide-neutral-200 gap-5">
+
+      <div className="mt-10 w-full border rounded-2xl p-4 border-neutral-300 divide-x divide-neutral-200 gap-5">
         {isLoading &&
-          Array.from({ length: 5 }).map((_, index) => (
-            <ProductCardSkeleton key={index} />
-          ))}
+          Array.from({ length: 55 }).map((_, index) => <ProductCardSkeleton />)}
 
         {!isLoading && error && (
           <p className="col-span-5 text-center text-red-500 py-8">{error}</p>
         )}
 
-        {!isLoading &&
-          products.length &&
-          products
-            .slice(0, 5)
-            .map((product) => <ProductCard key={product._id} {...product} />)}
+        {!isLoading && products.length && (
+          <Swiper
+            slidesPerView={5}
+            modules={[Autoplay]}
+            autoplay={{ delay: 1500, disableOnInteraction: false }}
+            breakpoints={{
+              300: {
+                slidesPerView: 1,
+              },
+              640: {
+                slidesPerView: 3,
+              },
+              768: {
+                slidesPerView: 4,
+              },
+              1024: {
+                slidesPerView: 5,
+              },
+              1280: {
+                slidesPerView: 5,
+              },
+            }}
+            loop
+          >
+            {products.map((product) => (
+              <SwiperSlide key={product._id}>
+                <ProductCard {...product} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </section>
   );
