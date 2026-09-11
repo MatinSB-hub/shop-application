46481883095ec7bb8f535ab6be12c3cc20a751c6
MatinSB-hub <matin.sa200@gmail.com>
2026-09-11 20:15:57 +0430

    feat(productCard): show product image
    - display product image
    - add default image as fallback for missing images

--- src/Components/Common/Cards/ProductCard/index.jsx
+++ src/Components/Common/Cards/ProductCard/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router";
 
-const ProductCard = ({ name, _id, sellers }) => {
+const ProductCard = ({ name, _id, sellers, images }) => {
   const price = sellers && sellers[0].price;
   return (
     <article className="w-full h-64 relative space-y-3 flex pl-2 justify-between flex-col">
@@ -8,7 +8,11 @@ const ProductCard = ({ name, _id, sellers }) => {
 
       <header className="h-34.5 flex-center w-full">
         <img
-          src="/assets/static/product1.png"
+          src={
+            images
+              ? `https://shopino.iran.liara.run/images/products/${images && images[0]}`
+              : "/assets/static/product1.png"
+          }
           className="h-full object-cover"
         />
       </header>
@@ -17,9 +21,7 @@ const ProductCard = ({ name, _id, sellers }) => {
       </main>
       <footer className="mt-auto! flex items-end justify-end">
         <div className="flex-ic gap-1">
-          <p className="font-black">
-            {Number(price).toLocaleString("fa-IR")}
-          </p>
+          <p className="font-black">{Number(price).toLocaleString("fa-IR")}</p>
           <div className="text-slate-600 text-xs">تومان</div>
         </div>
       </footer>
--- src/Components/Templates/Product/Sidebar/Fragments/Price.jsx
+++ src/Components/Templates/Product/Sidebar/Fragments/Price.jsx
@@ -2,7 +2,7 @@ const Price = ({ price = 10_750_000 }) => {
   return (
     <div className="flex gap-1 items-center justify-end">
       <span className="text-xl text-slate-700 font-bold">
-        <strong>{price}</strong>
+        <strong>{price.toLocaleString("fa-IR")}</strong>
       </span>
       <span className="text-sm font-bold text-slate-700">تومان</span>
     </div>
